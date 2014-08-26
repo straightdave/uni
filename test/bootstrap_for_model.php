4c5757588e2d82c1e3a68b48d7383ee1a1048774
straightdave <eyaswoo@gmail.com>
2014-08-26 14:33:39 +0800

    bootstrap for model testing

---
+++ test/bootstrap_for_model.php
@@ -0,0 +1,24 @@
+<?php
+// load Eloquent
+require_once('vendor/autoload.php'); 
+
+// Database information
+$settings = array(
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'uni_dev',
+    'username'  => 'root',
+    'password'  => '123123',
+    'collation' => 'utf8_general_ci',
+    'charset'   => 'utf8',
+    'prefix'    => ''
+);
+
+// Bootstrap Eloquent ORM
+$connFactory = new \Illuminate\Database\Connectors\ConnectionFactory(new \Illuminate\Container\Container);
+$conn        = $connFactory->make($settings);
+$resolver    = new \Illuminate\Database\ConnectionResolver();
+$resolver->addConnection('default', $conn);
+$resolver->setDefaultConnection('default');
+\Illuminate\Database\Eloquent\Model::setConnectionResolver($resolver);
+
--- test/firstTest.php
+++ test/firstTest.php
@@ -2,9 +2,29 @@
 
 class MyTest extends PHPUnit_Framework_TestCase {
 
-    public function testHaha() {
+    public function test_login_smoke() {
         $resp = \Httpful\Request::get('http://localhost/login')->send();
-        $this->assertNotEquals($resp, null);
+        $this->assertNotNull($resp, 'response is null');
+    }
+
+    public function test_login_return_temp_token() {
+        $resp = \Httpful\Request::post(
+                                        'http://localhost/login',
+                                        'username=dave&password=123123',
+                                        'application/x-www-form-urlencoded'
+                                      )->send();
+        $this->assertNotNull($resp, 'response is null: ');
+        $this->assertNotFalse($resp->code >= 300 and $resp->code < 400, 'response is not a redirection');
+        $url = $resp->headers['location'];
+        $this->assertNotFalse( strpos($url, 't='), $url . ' not including a token param');
+    }
+
+    public function test_model() {
+        require_once('test/bootstrap_for_model.php');
+        require_once('app/models/UserLogin.php');
+        
+        $users = UserLogin::all();
+        $this->assertNotFalse($users->count() > 0, 'failed to get users via model');
     }
 
 
