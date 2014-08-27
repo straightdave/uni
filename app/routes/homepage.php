a2806b0ed904abe4935d36bc153bbdfbc0397cc7
straightdave <eyaswoo@gmail.com>
2014-08-27 15:49:53 +0800

    fix some isue after keeping one user login

--- app/routes/homepage.php
+++ app/routes/homepage.php
@@ -1,25 +1,36 @@
 <?php
 
 $app->get('/', function () use($app) {
-    if( isset($_COOKIE['uniqueid'])) {
-        $uid = $_COOKIE['uniqueid'];
-        $sess = UserSession::where('id', '=', $uid)->firstOrFail();
-        $now = new DateTime('now');
-        $exp = new DateTime( $sess->exp );
-        if( $now < $exp ) {
-            $user = UserLogin::where('id', '=', $sess->uid)->firstOrFail();
-            $app->render('homepage.php', array( 
-                'url' => $app->urlFor('logout') . '?t=' . $sess->token . '&ret=/',
-                'name' => $user->name
-            ));
+    try{
+        if( isset($_COOKIE['uniqueid'])) {
+            $uid = $_COOKIE['uniqueid'];
+
+            // this uid could not be found in db (if loggout somewhere else)
+            $sess = UserSession::where('id', '=', $uid);
+            if( $sess->count() == 1 ) {
+                $sess = $sess->first();
+                $now = new DateTime('now');
+                $exp = new DateTime( $sess->exp );
+                if( $now < $exp ) {
+                    $user = UserLogin::where('id', '=', $sess->uid)->firstOrFail();
+                    $app->render('homepage.php', array(
+                        'url' => $app->urlFor('logout') . '?t=' . $sess->token . '&ret=/',
+                        'name' => $user->name
+                    ));
+                    return;
+                }
+            }
         }
-        return;
+
+        $app->render('homepage.php', array(
+            'url' => $app->urlFor('login'),
+            'name' => ''
+        ));
     }
-    
-    $app->render('homepage.php', array( 
-        'url' => $app->urlFor('login'),
-        'name' => ''
-    ));
+    catch(Exception $e) {
+        $app->flash( 'error', $e->getMessage() );
+        $app->redirect('/error');
+    }
 });
 
 $app->get('/error', function () use($app) {
--- app/views/error.php
+++ app/views/error.php
@@ -5,7 +5,7 @@
 <body>
 <h1>Oops! An error occurred.:-(</h1>
 <h3>Message:</h3>
-<p><?php echo($_SESSION['slim.flash']['error']); ?></p>
+<p><?php if(isset($_SESSION['slim.flash']['error'])) echo($_SESSION['slim.flash']['error']); else echo('no error!'); ?></p>
 <p><a href="/">Back to homepage</a></p>
 </body>
 <html>
