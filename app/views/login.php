baf08838f9d15e18f4eda27dd57a3e190fed034b
straightdave <eyaswoo@gmail.com>
2014-08-28 10:42:25 +0800

    add feature: remember me 10 days

--- app/views/login.php
+++ app/views/login.php
@@ -72,10 +72,11 @@
       <p class="title">Log in</p>
       <p class="error"><?php if( isset($errorMessage) ) {echo $errorMessage;} ?></p>
       <form method="post">
-      <div class="lbfield">Username:</div>
+      <div class="lbfield">Username</div>
       <div class="inputfield"><input type="text" name="username" value="dave"></div> <br/>
-      <div class="lbfield">Password:</div>
+      <div class="lbfield">Password</div>
       <div class="inputfield"><input type="password" name="password" value="123123"></div> <br/>
+      <input type="checkbox" name="rememberme" />Remember Me <br/>
       <input type="submit" value="Submit!">
       <a href="/signup">Sign up!</a>
       </form>
